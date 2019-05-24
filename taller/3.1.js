a1b595aea4f825798d655e50ef3f21ad03aa0a3f
equiman <equiman@users.noreply.github.com>
2019-05-24 11:22:04 -0500

    Add workshop 3

---
+++ taller/3.1.js
@@ -0,0 +1,13 @@
+//Taller 3: Propiedades
+const persona = {
+    nombre: "Homer J Simpson",
+    edad: 39,
+    residencia: {
+        direccion: "Avenida Siempreviva 742",
+        ciudad: "Springfield"
+    },
+    esEmpleado: true,
+    hijos: ["Bart", "Lisa", "Maggie"]
+}
+
+//TODO: Mostrar cada propiedad
