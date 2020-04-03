41fafdda8b0fa0821529c7564d4be96a5569d43e
equiman <equiman@users.noreply.github.com>
2020-04-02 23:36:56 -0500

    Stringify advanced

--- taller/2.3.js
+++ taller/2.3.js
@@ -1,11 +1,12 @@
 const edad = () => 18;
+const calcularFecha = () => (new Date()).toJSON();
 
 // Taller 2.3: Conversiones
 const maltrecho = {
     esValido: false,
     calcular: edad(),
-    fecha: (new Date()).toJSON()
+    fecha: calcularFecha()
 }
-const texto = JSON.stringify(maltrecho);
+const texto = JSON.stringify(maltrecho, ['calcular', 'fecha'], 2);
 
 console.log(texto);
