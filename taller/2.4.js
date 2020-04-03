ff68494907348b32f59a76f2a2fca66c0be5e290
equiman <equiman@users.noreply.github.com>
2020-04-02 23:37:04 -0500

    Parse advanced

--- taller/2.4.js
+++ taller/2.4.js
@@ -1,5 +1,18 @@
 // Taller 2.4: Conversiones
-const texto = '{"esValido": false, "calcular": edad(), "fecha": (new Date()).toJSON()}';
-//TODO: Convertir a Objeto
+const conversion = (clave, valor) => {
+  if (typeof valor !== "string") {
+    return valor;
+  }
+
+  const isoDate = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+  if (isoDate.test(valor)) {
+    return Date.parse(valor);
+  }
+
+  return valor;
+};
+
+const texto = '{"esValido": false, "calcular": 18, "fecha": "2020-04-01T03:15:32.130Z"}';
+const maltrecho = JSON.parse(texto, conversion);
 
 console.log(maltrecho);
