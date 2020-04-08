0fa64d8c2c10097e04f0fba05568847d8a681fc8
equiman <equiman@users.noreply.github.com>
2020-04-07 21:58:01 -0500

    Fix path const

--- api/saveFile.js
+++ api/saveFile.js
@@ -1,24 +1,29 @@
-const path = require("path")
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 const toJSON = (objectToSave) => {
   try {
     const json = JSON.stringify(objectToSave, null, 4);
     const result = (err) => {
       if (err) throw err;
-      console.log('The file has been saved!');
-    }
-    const fileName = path.join(__dirname, '../', 'data/got.json');
+      console.log("The file has been saved!");
+    };
+    const fileName = path.join(__dirname, "../", "data/got.json");
 
-    fs.writeFile(fileName, json, {
-      encoding: 'utf8',
-      flag: 'w'
-    }, result);
+    fs.writeFile(
+      fileName,
+      json,
+      {
+        encoding: "utf8",
+        flag: "w",
+      },
+      result
+    );
   } catch (error) {
-    console.log('Error: ', error);
+    console.log("Error: ", error);
   }
-}
+};
 
 module.exports = {
-  toJSON
+  toJSON,
 };
