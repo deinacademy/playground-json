12ecdd8f077ac975a677e6c656680a4989c07479
equiman <equiman@users.noreply.github.com>
2020-04-12 16:05:07 -0500

    Update got API

--- api/got.js
+++ api/got.js
@@ -1,29 +1,21 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const server = 'https://api.got.show';
 
 const rest = async (endpoint) => {
-    try {
-        let response = await fetch(`${server}/${endpoint}`);
-        let data = await response.json();
-        return Promise.resolve(data);
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+  let response = await fetch(`${server}/${endpoint}`);
+  let data = await response.json();
+  return data;
+};
 
 const request = (endpoint, callback) => {
-    rest(endpoint)
-        .then(
-            (result) => {
-                callback(result);
-            }
-        )
-        .catch(
-            (error) => console.error(error.message)
-        );
-}
+  rest(endpoint)
+    .then((result) => {
+      callback(result);
+    })
+    .catch((error) => console.error(error.message));
+};
 
 module.exports = {
-    request
+  request,
 };
